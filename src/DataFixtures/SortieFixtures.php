20705844d8da23b3578db273c530618b9a2b546a
Pierrick <pierrick.thomas2019@campus-eni.fr>
2020-04-16 14:53:30 +0200

    maj page home

--- src/DataFixtures/SortieFixtures.php
+++ src/DataFixtures/SortieFixtures.php
@@ -6,24 +6,25 @@ use App\Entity\Sortie;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use Doctrine\Persistence\ObjectManager;
+use http\Client\Curl\User;
 
 class SortieFixtures extends Fixture implements DependentFixtureInterface
 {
     public function load(ObjectManager $manager)
     {
-        for ($i = 0; $i <=10;$i++)
-        {
+        for ($i = 0; $i <= 10; $i++) {
             $sortie = new Sortie();
             $sortie->setNom("Sortie $i");
             $sortie->setDateHeureDebut(new \DateTime());
             $sortie->setDateLimiteInscription(new \DateTime('+1 day'));
-            $sortie->setNbInscriptionMax(rand(0,15));
-            $sortie->setDuree(rand(0,200));
+            $sortie->setNbInscriptionMax(rand(5, 8));
+            $sortie->setDuree(rand(0, 200));
             $sortie->setInfosSortie("Description de la sortie $i");
-            $sortie->setLieu($this->getReference('lieu_'.rand(0,3)));
-            $sortie->setUser($this->getReference('user_'.rand(0,4)));
-            $sortie->setEtat($this->getReference('etat_'.rand(0,5)));
-            $sortie->setSite($this->getReference('site_'.rand(0,2)));
+            $sortie->setLieu($this->getReference('lieu_' . rand(0, 3)));
+            $sortie->setUser($this->getReference('user_' . rand(0, 4)));
+            $sortie->setEtat($this->getReference('etat_' . rand(0, 5)));
+            $sortie->setSite($this->getReference('site_' . rand(0, 2)));
+            $sortie->addInscrit($sortie->getUser());
 
             $manager->persist($sortie);
         }
